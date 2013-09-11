68ddffa6e1925f473c2b117c2362e3ab35b17f06
idsc3101 <idsc3101@gmail.com>
2013-09-11 03:24:51 -0500

    Created project skeleton

---
+++ problem1/BMICalc/BMICalc/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtWeight = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.btnCalcBMI = New System.Windows.Forms.Button()
+        Me.txtBMI = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtWeight
+        '
+        Me.txtWeight.Location = New System.Drawing.Point(125, 34)
+        Me.txtWeight.Name = "txtWeight"
+        Me.txtWeight.Size = New System.Drawing.Size(130, 26)
+        Me.txtWeight.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(53, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Weight"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(53, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Height"
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Location = New System.Drawing.Point(125, 66)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(130, 26)
+        Me.txtHeight.TabIndex = 2
+        '
+        'btnCalcBMI
+        '
+        Me.btnCalcBMI.Location = New System.Drawing.Point(125, 118)
+        Me.btnCalcBMI.Name = "btnCalcBMI"
+        Me.btnCalcBMI.Size = New System.Drawing.Size(130, 33)
+        Me.btnCalcBMI.TabIndex = 4
+        Me.btnCalcBMI.Text = "Calculate BMI"
+        Me.btnCalcBMI.UseVisualStyleBackColor = True
+        '
+        'txtBMI
+        '
+        Me.txtBMI.AutoSize = True
+        Me.txtBMI.Location = New System.Drawing.Point(74, 185)
+        Me.txtBMI.Name = "txtBMI"
+        Me.txtBMI.Size = New System.Drawing.Size(38, 20)
+        Me.txtBMI.TabIndex = 6
+        Me.txtBMI.Text = "BMI"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(125, 182)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(130, 26)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(261, 37)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 20)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "lbs"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(261, 69)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 20)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "inches"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(384, 260)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtBMI)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnCalcBMI)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtWeight)
+        Me.Name = "Form1"
+        Me.Text = "BMI Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtWeight As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtHeight As System.Windows.Forms.TextBox
+    Friend WithEvents btnCalcBMI As System.Windows.Forms.Button
+    Friend WithEvents txtBMI As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ problem1/BMICalc/BMICalc/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub btnCalcBMI_Click(sender As System.Object, e As System.EventArgs) Handles btnCalcBMI.Click
+
+    End Sub
+End Class
